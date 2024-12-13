dc9ae9c63355accc57bfd9a451ea8d9dfe139d1b
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-12-13 21:44:32 +0600

    upgreade the yarn and new component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,28 @@
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        {/* left side */}
+        <div className="c-left">
+          <span className="orangeText">Our Contact Us</span>
+          <span className="primaryText">Easy to contact us</span>
+          <span className="secondaryText">
+            We always ready to help by providijng the best services for you.
+            <br /> We beleive a good blace to live can make your life better
+          </span>
+        </div>
+
+        {/* right side */}
+        <div className="c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
