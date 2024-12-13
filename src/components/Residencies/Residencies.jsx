aae54617644452a0f3f5b5a28bcd321497a5ee1d
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-12-13 08:49:17 +0600

    added new section called residencies

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -2,7 +2,7 @@ import './Residencies.css';
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import 'swiper/css';
 import data from '../../utils/slider.json';
-
+import { sliderSettings } from '../../utils/common';
 const Residencies = () => {
   return (
     <section className="r-wrapper">
@@ -11,23 +11,24 @@ const Residencies = () => {
           <span className="orangeText">Best Choices </span>
           <span className="primaryText">Popular Residencies</span>
         </div>
-        <Swiper>
-          {data.map((card, i) => (
-            <SwiperSlide key={i}>
+        <Swiper { ...sliderSettings }>
+          <SliderButtons />
+          { data.map((card, i) => (
+            <SwiperSlide key={ i }>
               <div className="flexColStart r-card">
-                <img src={card.image} alt="cardImg" width={240} />
+                <img src={ card.image } alt="cardImg" width={ 240 } />
 
                 <span className="secondaryText r-price">
-                  <span style={{ color: 'orange' }}>$</span>
-                  <span>{card.price}</span>
+                  <span style={ { color: 'orange' } }>$</span>
+                  <span>{ card.price }</span>
                 </span>
 
-                <span className="primaryText">{card.name}</span>
+                <span className="primaryText">{ card.name }</span>
 
-                <span className="secondaryText">{card.detail}</span>
+                <span className="secondaryText">{ card.detail }</span>
               </div>
             </SwiperSlide>
-          ))}
+          )) }
         </Swiper>
       </div>
     </section>
@@ -35,3 +36,15 @@ const Residencies = () => {
 };
 
 export default Residencies;
+
+const SliderButtons = () => {
+  const swiper = useSwiper()
+  return (
+    <div className='flexCenter r-buttons'>
+      <button onClick={ () => { swiper.slidePrev() } }>&lt;</button>
+      <button onClick={ () => { swiper.slideNext() } }>&gt;</button>
+    </div>
+
+
+  )
+}
---
+++ src/components/Value/Value.jsx
@@ -0,0 +1,23 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+  AccordionItemState
+} from 'react-accessible-accordion'
+import 'react-accessible-accordion/dist/fancy-example.css'
+import { MdOutlineArrowDropDown } from 'react-icons/md'
+import './Value.css'
+
+const Value = () => {
+  return (
+    <section className='v-wrapper'>
+      <div className="paddings innerWidth flexCenter v-container">
+
+      </div>
+    </section>
+  )
+}
+
+export default Value
